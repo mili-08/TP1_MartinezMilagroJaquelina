ff4c3aa489167517631a397b8047424b917f1fa7
mili-08 <milijmartinez@gmail.com>
2024-04-08 18:26:56 -0300

    Ejercicio 1

---
+++ TP1_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		int valor1=8, valor2=5, valor3=1, valor4=20, valor5=9;
+		double resultado = (valor1+valor2+valor3+valor4+valor5)/5.0;
+		System.out.println(valor1 + ", " + valor2 + ", " + valor3 + ", "+ valor4 + ", " + valor5 + "\nEl promedio es: " + resultado);
+	}
+
+}
