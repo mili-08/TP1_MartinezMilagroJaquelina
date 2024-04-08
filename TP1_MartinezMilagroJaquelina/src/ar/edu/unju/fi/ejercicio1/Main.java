9b276a2a38574277e5ce35f16f66bcc180a2c511
mili-08 <milijmartinez@gmail.com>
2024-04-08 18:47:25 -0300

    Se cambio el nombre de la clase del ejercicio 1 y se creo la clase 2

--- TP1_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ TP1_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -1,6 +1,6 @@
 package ar.edu.unju.fi.ejercicio1;
 
-public class Ejercicio1 {
+public class Main {
 
 	public static void main(String[] args) {
 		int valor1=8, valor2=5, valor3=1, valor4=20, valor5=9;
---
+++ TP1_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+	  String pais = "Argetina";
+	  byte edad = 20;
+	  Double altura = 1.63;
+	  Double precio = 78.9;
+      long telefono = 3885891321l;
+      Double coseno = Math.cos(0.5);
+      System.out.println("Pais: " + pais + "\nEdad: " + edad + "\nAltura: " + altura + "\nPrecio de un producto: " + precio + "\nNumero de telefono: " + telefono + "\nCoseno de 0,5: " + coseno);
+	}
+}
