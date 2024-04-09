eba381215960fb348065079785fb5ca3f307cede
mili-08 <milijmartinez@gmail.com>
2024-04-09 00:20:38 -0300

    Ejercicio 6

---
+++ TP1_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -0,0 +1,66 @@
+package ar.edu.unju.fi.ejercicio6.main;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio6.model.Persona;
+
+public class Main {
+
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		Persona persona1 = new Persona();
+		Long dni = solicitarDNI();
+		String nombre = solicitarNombre();
+		LocalDate fechaNac = solicitarFechNac();
+		String provincia = solicitarProvincia();
+
+		persona1.setDni(dni);
+		persona1.setFechaNacimiento(fechaNac);
+		persona1.setNombre(nombre);
+		persona1.setProvincia(provincia);
+		persona1.mostrarDatos();
+
+		dni = solicitarDNI();
+		nombre = solicitarNombre();
+		fechaNac = solicitarFechNac();
+		provincia = solicitarProvincia();
+
+		Persona persona2 = new Persona(dni, nombre, fechaNac, provincia);
+		persona2.mostrarDatos();
+
+		dni = solicitarDNI();
+		nombre = solicitarNombre();
+		fechaNac = solicitarFechNac();
+
+		Persona persona3 = new Persona(dni, nombre, fechaNac);
+		persona3.mostrarDatos();
+		sc.close();
+	}
+
+	public static Long solicitarDNI() {
+		System.out.println("\nIngrese el dni: ");
+		Long dni = sc.nextLong();
+		return dni;
+	}
+
+	public static String solicitarNombre() {
+		System.out.println("Ingrese el nombre: ");
+		String nombre = sc.next();
+		return nombre;
+	}
+
+	public static LocalDate solicitarFechNac() {
+		System.out.println("Ingrese la fecha en este formato 'yyyy-mm-dd': ");
+		LocalDate fechaNac = LocalDate.parse(sc.next());
+		return fechaNac;
+	}
+
+	public static String solicitarProvincia() {
+		System.out.println("Ingrese la provincia: ");
+		String provincia = sc.next();
+		return provincia;
+	}
+}
---
+++ TP1_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -0,0 +1,84 @@
+package ar.edu.unju.fi.ejercicio6.model;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Persona {
+	private Long dni;
+	private String nombre;
+	private LocalDate fechaNacimiento;
+	private String provincia;
+
+	public Persona() {
+
+	}
+    
+
+	public Persona(Long dni, String nombre, LocalDate fechaNacimiento, String provincia) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fechaNacimiento = fechaNacimiento;
+		this.provincia = provincia;
+	}
+
+    
+
+	public Persona(Long dni, String nombre, LocalDate fechaNacimiento) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.fechaNacimiento = fechaNacimiento;
+		this.provincia = "Jujuy";
+	}
+
+
+	public long getDni() {
+		return dni;
+	}
+
+	public void setDni(Long dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+    
+	public int calcularEdad () {
+		Period periodo = Period.between(this.fechaNacimiento, LocalDate.now());
+		return periodo.getYears();
+	}
+    
+	public boolean mayorEdad () {
+		return calcularEdad()>=18;
+	}
+	
+	public void mostrarDatos() {
+		System.out.println("\nNombre: " + nombre + "\nDNI: " + dni + "\nProvincia: " + provincia + "\nFecha de Nacimiento: " + fechaNacimiento);
+		if (mayorEdad()==true)
+			System.out.println("La persona es mayor de edad");
+		else 
+			System.out.println("La persona no es mayor de edad");
+	}
+}
