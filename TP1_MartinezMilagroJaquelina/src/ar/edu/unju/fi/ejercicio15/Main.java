975f7ad61af736cc9ee107c3b9335cc53a3e8b7f
mili-08 <milijmartinez@gmail.com>
2024-04-10 21:10:40 -0300

    Ejercicio 15

---
+++ TP1_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio15/Main.java
@@ -0,0 +1,63 @@
+package ar.edu.unju.fi.ejercicio15;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static Scanner sc= new Scanner(System.in);
+	
+	public static void main(String[] args) {
+	 int cantidad;
+	 do
+	 {
+		 cantidad = verificarEntero();
+		 if(!(cantidad>=5 && cantidad<=10))
+			 System.out.println("\nINVALIDO, DEBE SER UN VALOR DE 5 A 1O\n");
+	 }while(!(cantidad>=5 && cantidad<=10));
+	 
+	 String[] vector = new String[cantidad];
+	 
+	 System.out.println("\n*** INGRESO DE VALORES ***\n");
+	 for(int a=0;a<vector.length;a++) {
+		 System.out.print("Ingrese un nombre: ");
+		 vector[a]=sc.nextLine();
+	 }
+	 
+	 mostrarValores(vector);
+	 mostrarValoresInverso(vector);
+	 
+     sc.close();
+	}
+	
+	public static int verificarEntero() {
+		int valor = 0;
+		boolean band;
+		do {
+			band = false;
+			try {
+				System.out.print("Ingrese un valor del 5 al 10 de acuerdo a la cantidad de valores que desea ingresar: ");
+				valor = sc.nextInt();
+				sc.nextLine();
+				band = true;
+			} catch (InputMismatchException e) {
+				System.out.println("\nINVALIDO, DEBE SER UN VALOR ENTERO DE 5 A 10\n");
+				sc.nextLine();
+			}
+		} while (!band);
+		return valor;
+	}
+	
+	public static void mostrarValores(String[] vector) {
+		System.out.println("\n*** MOSTRANDO DATOS ***\n");
+		for(String valor: vector)
+		 System.out.println(valor);
+	}
+	
+	public static void mostrarValoresInverso (String[] vector) {
+		System.out.println("\n*** MOSTRANDO DATOS INVERSAMENTE***\n");
+		for(int a=vector.length-1;a>=0;a--)
+			System.out.println(vector[a]);
+	}
+
+}
